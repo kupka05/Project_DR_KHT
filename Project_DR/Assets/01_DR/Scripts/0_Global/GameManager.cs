79e07c243a5b94f0d928ff6c328a1d0525624864
Jihwan <jahniyoon@gmail.com>
2023-11-03 12:59:24 +0900

    [Jihwan] Add Project Files

---
+++ Project_DR/Assets/01_DR/Scripts/0_Global/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance
+    {
+        get
+        {
+            // 만약 싱글톤 변수에 아직 오브젝트가 할당되지 않았다면
+            if (m_instance == null)
+            {
+                // 씬에서 GameManager 오브젝트를 찾아 할당
+                m_instance = FindObjectOfType<GameManager>();
+            }
+
+            // 싱글톤 오브젝트를 반환
+            return m_instance;
+        }
+    }
+    private static GameManager m_instance; // 싱글톤이 할당될 static 변수    
+
+    void Start()
+    {
+      
+    }       // Start()
+
+    
+    void Update()
+    {
+
+
+    }       // Update()
+
+
+}       // ClassEnd

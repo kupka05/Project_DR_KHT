379f987229115788a352015ac346f92c821662a1
Jinseong_NB2 <sooyoung2762@gmail.com>
2023-11-22 20:49:17 +0900

    [Choi] Add: Item Manager Create Item Functions

--- Project_DR/Assets/01_DR/Scripts/3_JS/ItemManager.cs
+++ Project_DR/Assets/01_DR/Scripts/3_JS/ItemManager.cs
@@ -24,8 +24,11 @@ public class ItemManager : MonoBehaviour
     #region [+]
     private void Start()
     {
+        // 아이템 DB Init
         ItemDataManager.InitItemDB();
-        CreatePotion(5001, 99);
+
+        // 테스트용 포션 생성
+        CreateItem(5001, 99);
     }
 
     #endregion
@@ -38,9 +41,59 @@ public class ItemManager : MonoBehaviour
         _inventory = inventory;
     }
 
-    public void CreatePotion(int id, int amount = 1)
+    // 자동으로 타입을 찾아서 아이템을 생성
+    public void CreateItem(int id, int amount = 1)
+    {
+        // 생성할 아이템이 Potion 타입일 경우
+        if (ItemDataManager.SearchItemDB<PortionItemData>(id))
+        {
+            CreatePotionItem(id, amount);
+        }
+
+        // 생성할 아이템이 Bomb 타입일 경우
+        else if (ItemDataManager.SearchItemDB<BombItemData>(id))
+        {
+            CreateBombItem(id, amount);
+        }
+
+        // 생성할 아이템이 Material 타입일 경우
+        else if (ItemDataManager.SearchItemDB<MaterialItemData>(id))
+        {
+            CreateMaterialItem(id, amount);
+        }
+
+        // 생성할 아이템이 Quest 타입일 경우
+        else 
+        {
+            CreateQuestItem(id, amount);
+        }
+    }
+
+    // 포션 아이템 생성
+    public void CreatePotionItem(int id, int amount = 1)
     {
         PortionItemData data = ItemDataManager.SearchItemDB<PortionItemData>(id);
+        _inventory.Add(data, amount);
+    }
+
+    // 폭탄 아이템 생성
+    public void CreateBombItem(int id, int amount = 1)
+    {
+        BombItemData data = ItemDataManager.SearchItemDB<BombItemData>(id);
+        _inventory.Add(data, amount);
+    }
+
+    // 재료 아이템 생성
+    public void CreateMaterialItem(int id, int amount = 1)
+    {
+        MaterialItemData data = ItemDataManager.SearchItemDB<MaterialItemData>(id);
+        _inventory.Add(data, amount);
+    }
+
+    // 퀘스트 아이템 생성
+    public void CreateQuestItem(int id, int amount = 1)
+    {
+        QuestItemData data = ItemDataManager.SearchItemDB<QuestItemData>(id);
         _inventory.Add(data, amount);
     }
 

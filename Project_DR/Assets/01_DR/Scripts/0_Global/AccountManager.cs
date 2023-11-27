f05504389301fc0ca425d1d52d79d58f227fd157
Jinseong_NB2 <sooyoung2762@gmail.com>
2023-11-27 15:07:41 +0900

    [Choi] Update: Login System

--- Project_DR/Assets/01_DR/Scripts/0_Global/AccountManager.cs
+++ Project_DR/Assets/01_DR/Scripts/0_Global/AccountManager.cs
@@ -15,6 +15,7 @@ public class AccountManager : MonoBehaviour
     [SerializeField] TMP_Text description;
 
     [SerializeField] string url;
+    [SerializeField] string sceneName;
 
     public void LoginClick() => StartCoroutine(AccountCo("login"));
 
@@ -22,6 +23,29 @@ public class AccountManager : MonoBehaviour
 
     public void SaveClick() => StartCoroutine(AccountCo("save"));
 
+////// 기존
+    //IEnumerator AccountCo(string command)
+    //{
+    //    WWWForm form = new WWWForm();
+    //    form.AddField("command", command);
+    //    form.AddField("id", idInput.text);
+    //    form.AddField("password", passwordInput.text);
+    //    //form.AddField("mbti", infoInput.text);
+
+    //    UnityWebRequest www = UnityWebRequest.Post(url, form);
+
+    //    yield return www.SendWebRequest();
+    //    description.text = www.downloadHandler.text;
+    //    print(www.downloadHandler.text);
+
+    //    switch (www.downloadHandler.text)
+    //    {
+    //        case "Login Complete":
+    //            SceneManager.LoadScene("JH MainScene");
+    //            break;
+    //    }
+      
+    //}
     IEnumerator AccountCo(string command)
     {
         WWWForm form = new WWWForm();
@@ -30,21 +54,60 @@ public class AccountManager : MonoBehaviour
         form.AddField("password", passwordInput.text);
         form.AddField("mbti", infoInput.text);
 
-        UnityWebRequest www = UnityWebRequest.Post(url, form);
-
-        yield return www.SendWebRequest();
-        description.text = www.downloadHandler.text;
-        print(www.downloadHandler.text);
-
-        switch (www.downloadHandler.text)
+        // using문을 사용하여 메모리 누수 해결
+        // 사용이 끝난 후 할당 해제
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
-            case "Login Complete":
-                SceneManager.LoadScene("JH MainScene");
-                break;
-        }
-       
+            yield return www.SendWebRequest();
 
+            print(www.downloadHandler.text);
+
+            switch (www.downloadHandler.text)
+            {
+                // 로그인 성송기
+                case "Login Complete":
+                    SceneManager.LoadScene(sceneName);
+                    break;
+            }
+
+            // using문을 사용해도 메모리 누수가 발생하여
+            // 추가로 Dipose()함수를 호출해서 할당 해제
+            www.Dispose();
+        }
     }
+
+
+
+    //IEnumerator AccountCo(string command)
+    //{
+    //    WWWForm form = new WWWForm();
+    //    form.AddField("command", command);
+    //    form.AddField("id", idInput.text);
+    //    form.AddField("password", passwordInput.text);
+    //    form.AddField("mbti", infoInput.text);
+    //    //form.AddField("password", passwordInput.text);
+    //    //form.AddField("info", infoInput.text);
+
+    //    // using문을 사용하여 메모리 누수 해결
+    //    // 사용이 끝난 후 할당 해제
+    //    using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+    //    {
+    //        yield return www.SendWebRequest();
+
+    //        if (www.result != UnityWebRequest.Result.Success)
+    //        {
+    //            Debug.Log(www.error);
+    //        }
+    //        else
+    //        {
+    //            print(www.downloadHandler.text);
+    //        }
+
+    //        // using문을 사용해도 메모리 누수가 발생하여
+    //        // 추가로 Dipose()함수를 호출해서 할당 해제
+    //        www.Dispose();
+    //    }
+    //}
 }
 
 

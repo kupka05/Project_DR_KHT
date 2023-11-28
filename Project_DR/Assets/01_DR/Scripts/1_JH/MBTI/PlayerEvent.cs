4525206425121ddc040f7680ebdf481e85916c48
Jihwan <jahniyooin@gmail.com>
2023-11-28 15:10:24 +0900

    [Jihwan] Fix Damage harm

--- Project_DR/Assets/01_DR/Scripts/1_JH/MBTI/PlayerEvent.cs
+++ Project_DR/Assets/01_DR/Scripts/1_JH/MBTI/PlayerEvent.cs
@@ -1,4 +1,5 @@
 using BNG;
+using Rito.InventorySystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,8 +28,27 @@ public class PlayerEvent : MonoBehaviour
         {
             grabItem.GetComponent<PlayerInventory>().OpenInventory();
         }
-    }
+        // 아이템 슬롯인지 확인
+        if(grabItem.GetComponent<ItemSlotController>() != null)
+        {
+            // 콜라이더가 인벤토리 스크롤 패널 안에 있을 경우
+            if (CheckColliderVisibility(
+                grabItem.transform.parent.parent.parent.parent.parent.parent.GetComponent<RectTransform>(),
+                grabItem.GetComponent<RectTransform>()) == true)
+            {
+                Debug.Log($"name: {grabItem.transform.parent.name}");
 
+                // 상태 변경
+            }
+        }
+    }
+    private bool CheckColliderVisibility(RectTransform scrollPanel, RectTransform other)
+    {
+        // 현재 객체가 스크롤 패널 내에 있는지 여부 확인
+        bool isVisible = RectTransformUtility.RectangleContainsScreenPoint(scrollPanel, other.position);
+
+        return isVisible;
+    }
     // 아이템 놓는 상태
     public void ReleaseItem(Grabbable grabItem)
     {

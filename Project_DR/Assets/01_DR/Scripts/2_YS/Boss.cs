6c18061a9bbb2003d2786590f20848ab3f3ab6e1
jinyoungsoo <rokjinyoungsoo@gmail.com>
2023-12-08 09:40:32 +0900

    [jinyoungsoo]보스 hp바 추가

--- Project_DR/Assets/01_DR/Scripts/2_YS/Boss.cs
+++ Project_DR/Assets/01_DR/Scripts/2_YS/Boss.cs
@@ -16,7 +16,7 @@ public class Boss : MonoBehaviour
 
     public UnityEngine.UI.Slider bossHPSlider;
 
-    public GameObject boss;
+    public GameObject bossState;
 
     public Rigidbody rigid;
     public Damageable damageable;
@@ -67,12 +67,7 @@ public class Boss : MonoBehaviour
     public Transform bigBrickPortRight;
     public GameObject bigBrick;
 
-    void Update()
-    {
-        
-
-        
-    }
+    
     void Awake()
     {
         GetData(bossId);
@@ -85,7 +80,7 @@ public class Boss : MonoBehaviour
     void InitializeBoss()
     {
         Debug.Log($"게임시작");
-        boss =  GameObject.FindWithTag("Boss");
+        bossState =  GameObject.FindWithTag("Boss");
 
         rigid = GetComponent<Rigidbody>();
         target = GameObject.FindWithTag("Player").GetComponent<PlayerPosition>().playerPos;
@@ -93,16 +88,14 @@ public class Boss : MonoBehaviour
 
         SetMaxHealth(maxHp);
 
-
-        //transform.LookAt(target.position);
     }
 
     void FixedUpdate()
     {
        if(!target)
-        {
-            return;
-        }
+       {
+          return;
+       }
             // Look At Y 각도로만 기울어지게 하기
             Vector3 targetPostition =
                 new Vector3(target.position.x, this.transform.position.y, target.position.z);
@@ -247,13 +240,16 @@ public class Boss : MonoBehaviour
                 BigBrickShoot();
                 break;
             case 1:
-                PlayShoot();
+                //StartCoroutine(PlayShoot());
+                BigBrickShoot();
                 break;
             case 2:
-                BounceShoot();
+                //BounceShoot();
+                BigBrickShoot();
                 break;
             case 3:
-               ExplosionShoot();
+                //ExplosionShoot();
+                BigBrickShoot();
                 break;
         }
     }
@@ -443,9 +439,9 @@ public class Boss : MonoBehaviour
                 // 이벤트 호출
                 unityEvent?.Invoke();
 
-                if (boss)
+                if (bossState)
                 {
-                    boss.GetComponent<BossState>().Die();
+                    bossState.GetComponent<BossState>().Die();
                 }
                 StopAllCoroutines();
             }

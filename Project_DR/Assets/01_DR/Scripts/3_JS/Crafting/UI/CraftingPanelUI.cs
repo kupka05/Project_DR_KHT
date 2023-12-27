58e0727a884b70467c187eba52aaae3fb411dedf
Jinseong_NB2 <sooyoung2762@gmail.com>
2023-12-27 22:07:33 +0900

    [Choi] Update: Crafting & GFunc SumString

--- Project_DR/Assets/01_DR/Scripts/3_JS/Crafting/UI/CraftingPanelUI.cs
+++ Project_DR/Assets/01_DR/Scripts/3_JS/Crafting/UI/CraftingPanelUI.cs
@@ -31,7 +31,7 @@ namespace Js.Crafting
             RectTransform rectTransform = GetComponent<RectTransform>();
             rectTransform.localPosition = Vector3.zero;
             rectTransform.localScale = Vector3.one;
-            _text.text = Data.GetString(id, "Text");
+            _text.text = ConvertText(id);
         }
 
 
@@ -39,11 +39,47 @@ namespace Js.Crafting
          *               Private Methods
          *************************************************/
         // 크래프팅에 맞게 텍스트를 변경 후 반환한다.
-        private void SetText(int id)
+        private string ConvertText(int id)
         {
-            
+            string itemName = Data.GetString(id, "Name");
+            string text = GFunc.SumString(itemName, ":");
+            int[] conditionKeyIDs =
+            {
+               Data.GetInt(id, "Condition_1_KeyID"),
+               Data.GetInt(id, "Condition_2_KeyID"),
+               Data.GetInt(id, "Condition_3_KeyID"),
+               Data.GetInt(id, "Condition_4_KeyID"),
+            };
+            for (int i = 0; i < conditionKeyIDs.Length; i++)
+            {
+                // 조건 ID가 0일 경우 무시하기
+                if (conditionKeyIDs[i].Equals(0)) { continue; }
+
+                int conditionID = conditionKeyIDs[i];
+                int material_1_KeyID = Data.GetInt(conditionID, "Material_1_KeyID");
+                int material_2_KeyID = Data.GetInt(conditionID, "Material_2_KeyID");
+                int material_1_Amount = Data.GetInt(conditionID, "Material_1_Amount");
+                int material_2_Amount = Data.GetInt(conditionID, "Material_2_Amount");
+                string itemName_1 = Data.GetString(material_1_KeyID, "Name");
+                string itemName_2 = Data.GetString(material_2_KeyID, "Name");
+                text += AddTextIfIDNotZero(material_1_KeyID, itemName_1, material_1_Amount);
+                text += AddTextIfIDNotZero(material_2_KeyID, itemName_2, material_2_Amount);
+            }
+
+            // 마지막 글자 삭제 후 반환
+            return text.Substring(0, text.Length - 1);
         }
 
-        // 조건식
+        // id가 0이 아닐 경우 텍스트 추가
+        private string AddTextIfIDNotZero(int id, string name, int amount)
+        {
+            string text = default;
+            if (! id.Equals(0))
+            {
+                text = GFunc.SumString(" " , name, "<size=18>x", amount.ToString(), "</size> +");
+            }
+
+            return text;
+        }
     }
 }

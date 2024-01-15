bedfdb598d9e30f8e9e3fb6562c49e6ab562dbc8
Jihwan <jahniyoon@gmail.com>
2024-01-15 14:20:10 +0900

    [Jihwan] Add Tutorial Scene

---
+++ Project_DR/Assets/01_DR/Scripts/1_JH/Tutorial/TutorialExit.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static TutorialExit;
+
+public class TutorialExit : MonoBehaviour
+{
+    public VRSceneLoder sceneLoader;
+    public enum Level { Easy, Hard}
+    public Level level;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            switch(level)
+            {
+                case Level.Easy:
+                    UserDataManager.Instance.Gold = 5000;
+                    UserDataManager.Instance.Exp = 30000;
+                    UserDataManager.Instance.SaveGoldandExp();
+                    break;
+                case Level.Hard:
+                    UserDataManager.Instance.Gold = 500;
+                    UserDataManager.Instance.Exp = 5000;
+                    UserDataManager.Instance.SaveGoldandExp();
+                    break;
+            }
+            sceneLoader.LoadScene();            
+        }
+    }
+
+}
